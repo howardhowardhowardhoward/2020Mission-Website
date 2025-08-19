995b66584402b6f4aedd6209ea9c9ad871d0de1c
howardzhang <zhanghoward2016@gmail.com>
2025-08-19 00:14:39 -0700

    configured for railway deployment

--- Website/__init__.py
+++ Website/__init__.py
@@ -10,7 +10,7 @@ def create_app():
         host= os.getenv('DB_HOST'),
         user= os.getenv('DB_USER'),
         password= os.getenv('DB_PASS'),
-        port=int(os.getenv('DB_PORT')),
+        port=int(os.getenv('DB_PORT', 3306)),
         database= os.getenv('DB_NAME')
     )
 
